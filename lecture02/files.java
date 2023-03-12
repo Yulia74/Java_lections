a8bf8cefc15ee0e5cc797d3d141df36ac10b932f
[Yulia Lebedeva] <yl.75@mail.ru>
2023-03-12 19:50:09 +0500

    add is done

---
+++ lecture02/files.java
@@ -0,0 +1,20 @@
+package lecture02;
+
+import java.io.File;
+
+public class files {
+    public static void main(String[] args) {
+        String pathProject = System.getProperty("user.dir");
+        String pathDir = pathProject.concat("/files");
+        File dir = new File(pathDir);
+        System.out.println(dir.getAbsolutePath());
+        if (dir.mkdir()) {
+            System.out.println("+");
+        } else {
+            System.out.println("-");
+        }
+        for (String fname : dir.list()) {
+            System.out.println(fname);
+        }
+    }
+}

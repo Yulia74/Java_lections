ce6a3d2f54d9e7bbcf68b09e387a1f61e1d48644
[Yulia Lebedeva] <yl.75@mail.ru>
2023-03-20 20:33:50 +0500

    ex_HashMap is add

---
+++ lecture05/ex_HashMap.java
@@ -0,0 +1,25 @@
+package lecture05;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ex_HashMap {
+    public static void main(String[] args) {
+        Map<Integer, String> db = new HashMap<>();
+        db.put(1, "один");
+        System.out.println(db);
+        db.put(2, "два");
+        System.out.println(db);
+        db.put(3, "три");
+        System.out.println(db);
+        db.put(31, "три один");
+        System.out.println(db);
+        db.put(13, "один три");
+        System.out.println(db);
+        db.put(null, "!null");
+        System.out.println(db);
+        db.put(null, null);
+        System.out.println(db);
+
+    }
+}
